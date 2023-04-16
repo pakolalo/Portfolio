51ce738324ce1c4d2f219f55b05c3d442d8ec6cd
Isco del Collado <113213836+pakolalo@users.noreply.github.com>
2023-04-16 15:46:24 -0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+// Add event listener on multiple elements
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+
+// Preloader
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+})
+
+// Navbar
+// navbar toggle for mobile 
+
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const navToggleBtn = document.querySelector('[data-nav-toggle-btn]');
+const navbar = document.querySelector('[data-navbar]');
+const overlay = document.querySelector('[data-overlay]');
+
+const toggleNavBar = function () {
+    navbar.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavBar);
+
+// Header
+// Header active when window scroll down to 100px
+
+const header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+});
+
+// Slider
+
+const sliders = document.querySelectorAll('[data-slider]');
+const initSlider = function (currentSlider) {
+    const sliderContainer = currentSlider.querySelector('[data-slider-container]');
+    const sliderPrevBtn = currentSlider.querySelector('[data-slider-prev]');
+    const sliderNextBtn = currentSlider.querySelector('[data-slider-next]');
+
+    let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue('--slider-items'));
+    let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    let currentSlidePos = 0;
+
+    const moveSliderItem = function () {
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+    }
+
+// Next slide
+
+    const slideNext = function () {
+        const slideEnd = currentSlidePos >= totalSlidableItems;
+        if (slideEnd) {
+            currentSlidePos = 0;
+        } else {
+            currentSlidePos++;
+        }
+        moveSliderItem();
+    }
+    sliderNextBtn.addEventListener('click',slideNext);
+
+// Previous slide
+
+    const slidePrev = function () {
+        if (currentSlidePos <= 0) {
+            currentSlidePos = totalSlidableItems;
+        } else {
+            currentSlidePos--;
+        }
+        moveSliderItem();
+    }
+    sliderPrevBtn.addEventListener('click',slidePrev);
+
+    const dontHaveExtraItem = totalSlidableItems <= 0;
+    if (dontHaveExtraItem) {
+        sliderNextBtn.style.display = 'none';
+        sliderPrevBtn.style.display = 'none';
+    }
+
+    //Slide with [shift + mouse wheel]
+
+    currentSlider.addEventListener('wheel', function (event) {
+        if (event.shiftKey && event.deltaY > 0) slideNext();
+        if (event.shiftKey && event.deltaY < 0) slidePrev();
+    });
+
+// Responsive    
+
+window.addEventListener("resize", function () {
+    totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue('--slider-items'));
+    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    moveSliderItem();
+})
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) {initSlider(sliders[i]); }
